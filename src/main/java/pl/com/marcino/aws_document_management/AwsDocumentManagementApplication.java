7a2c048fda0a1bed40b599167a64ddfea22ad4ca
Marcin O <seldo97@gmail.com>
2024-06-19 00:09:31 +0200

    init project

---
+++ src/main/java/pl/com/marcino/aws_document_management/AwsDocumentManagementApplication.java
@@ -0,0 +1,13 @@
+package pl.com.marcino.aws_document_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDocumentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDocumentManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/marcino/aws_document_management/AwsDocumentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.marcino.aws_document_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDocumentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
